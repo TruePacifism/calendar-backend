380e8fd6667b43a224e66d788fda5ceb8dc33eb2
TruePacifism <gorov2323@mail.ru>
2023-11-22 23:15:05 +0300

    Закомментил неработающий код

--- dist/src/commanders/countingCardData/recountAllData.js
+++ dist/src/commanders/countingCardData/recountAllData.js
@@ -1,42 +1,30 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Schemas_1 = require("../db/Schemas");
-const getCardData_1 = __importDefault(require("./getCardData"));
-function recountAllData() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const users = yield Schemas_1.UserModel.find({});
-        users.forEach((user) => __awaiter(this, void 0, void 0, function* () {
-            user.cards = yield Promise.all(user.cards.map((card) => __awaiter(this, void 0, void 0, function* () {
-                const { birthcity, birthdate, gender, livingcity, name } = card;
-                if (gender !== "male" && gender !== "" && gender !== "female") {
-                    return;
-                }
-                const newCard = yield (0, getCardData_1.default)({
-                    birthcity,
-                    birthdate,
-                    gender,
-                    livingcity,
-                    name,
-                });
-                card = Object.assign(Object.assign({}, card), newCard);
-                return Object.assign(Object.assign({}, card), newCard);
-            })));
-            user.save();
-        }));
-        return "Все карты пересчитаны";
-    });
-}
-exports.default = recountAllData;
+// import { UserModel } from "../db/Schemas";
+// import getCardData from "./getCardData";
+// export default async function recountAllData(): Promise<string> {
+//   const users = await UserModel.find({});
+//   users.forEach(async (user) => {
+//     user.cards = await Promise.all(
+//       user.cards.map(async (card) => {
+//         const { birthcity, birthdate, gender, livingcity, name } = card;
+//         if (gender !== "male" && gender !== "" && gender !== "female") {
+//           return;
+//         }
+//         const newCard = await getCardData({
+//           birthcity,
+//           birthdate,
+//           gender,
+//           livingcity,
+//           name,
+//         });
+//         card = {
+//           ...card,
+//           ...newCard,
+//         };
+//         return { ...card, ...newCard };
+//       })
+//     );
+//     user.save();
+//   });
+//   return "Все карты пересчитаны";
+// }
 //# sourceMappingURL=recountAllData.js.map
--- src/commanders/countingCardData/recountAllData.ts
+++ src/commanders/countingCardData/recountAllData.ts
@@ -1,30 +1,30 @@
-import { UserModel } from "../db/Schemas";
-import getCardData from "./getCardData";
+// import { UserModel } from "../db/Schemas";
+// import getCardData from "./getCardData";
 
-export default async function recountAllData(): Promise<string> {
-  const users = await UserModel.find({});
-  users.forEach(async (user) => {
-    user.cards = await Promise.all(
-      user.cards.map(async (card) => {
-        const { birthcity, birthdate, gender, livingcity, name } = card;
-        if (gender !== "male" && gender !== "" && gender !== "female") {
-          return;
-        }
-        const newCard = await getCardData({
-          birthcity,
-          birthdate,
-          gender,
-          livingcity,
-          name,
-        });
-        card = {
-          ...card,
-          ...newCard,
-        };
-        return { ...card, ...newCard };
-      })
-    );
-    user.save();
-  });
-  return "Все карты пересчитаны";
-}
+// export default async function recountAllData(): Promise<string> {
+//   const users = await UserModel.find({});
+//   users.forEach(async (user) => {
+//     user.cards = await Promise.all(
+//       user.cards.map(async (card) => {
+//         const { birthcity, birthdate, gender, livingcity, name } = card;
+//         if (gender !== "male" && gender !== "" && gender !== "female") {
+//           return;
+//         }
+//         const newCard = await getCardData({
+//           birthcity,
+//           birthdate,
+//           gender,
+//           livingcity,
+//           name,
+//         });
+//         card = {
+//           ...card,
+//           ...newCard,
+//         };
+//         return { ...card, ...newCard };
+//       })
+//     );
+//     user.save();
+//   });
+//   return "Все карты пересчитаны";
+// }
